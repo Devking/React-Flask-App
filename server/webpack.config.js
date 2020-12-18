8c48c8b8a11455b0a2d69fdc451f37fecd7fd1fd
Wells Lucas Santo <theonlydevking@gmail.com>
2020-12-18 05:28:52 -0800

    Create a reasonable file structure

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'static'),
   },
   module: { rules: [
     {
